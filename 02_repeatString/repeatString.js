1afa5ee65f4368a0121cd79f87a8d7ee5c60b04b
Faiz <76619765+FaizNajib16@users.noreply.github.com>
2024-08-08 09:47:13 +0800

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (times < 0) return "ERROR";
+    let appendString = string;
+    for (let i = 1; i < number; i++) {
+        string += appendString;
+    }
+    return string;
 };
 
 // Do not edit below this line
