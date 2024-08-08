aac3fc4cd67a36dbc2fb4b5086df9578e27bfd32
Faiz <76619765+FaizNajib16@users.noreply.github.com>
2024-08-08 11:23:11 +0800

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    const newArray = [];
+    const args = Array.from(theArgs);
+
+    for(index of array){
+        if(!args.includes(index)){
+            newArray.push(index);  
+        }
+        // newArray.push(index);  
+    }
+    return newArray;
 };
 
 // Do not edit below this line
