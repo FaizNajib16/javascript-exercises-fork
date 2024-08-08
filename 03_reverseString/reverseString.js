345ba35f322a5deb000bf248efcd2e8002f96152
Faiz <76619765+FaizNajib16@users.noreply.github.com>
2024-08-08 10:20:56 +0800

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    word = word.split("");
+    let reverseWord = "";
+    for(letters of word){
+        reverseWord = letters + reverseWord; 
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
