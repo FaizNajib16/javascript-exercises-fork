bf348a01ade2ba6b4ec736c2ab91ea6ae4354851
Faiz <76619765+FaizNajib16@users.noreply.github.com>
2024-08-09 14:26:10 +0800

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    if(number1 < 0 || number2 < 0 || !Number.isInteger(number1) || !Number.isInteger(number2)){
+        return "ERROR";
+    }
+    
+    let sum = 0;
+
+    // Swap numbers if number1 is greater than number2
+    if(number1 > number2){
+        const temp = number1;
+        number1 = number2;
+        number2 = temp;
+    }
+
+    for(let number = number1; number <= number2; ++number){
+        sum += number;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*
+START
+FUNCTION sumAll takes two numbers
+    SET sum to 0
+    FOR the range of number from number 1 to number 2
+        INCREMENT sum by number
+    ENDFOR
+    RETURN sum
+END FUNCTION
+END
+
+*/
